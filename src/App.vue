8986f5965233123ab05fb9048e2f799f15a77600
nonso01 <chukwunonsomartin01@gmail.com>
2024-09-27 18:50:34 +0100

    use icon lib

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import FallLimited from './components/FallLimited.vue';
+</script>
 
 <template>
-  <h1>E commerce site</h1>
+  <!-- <h1>E commerce site</h1> -->
+   <FallLimited />
 </template>
 
 <style scoped></style>
---
+++ src/components/FallLimited.vue
@@ -0,0 +1,45 @@
+<script setup>
+import IconPlus from '/icon-plus.svg'
+</script>
+
+<template>
+  <div class="f-l">
+    <div class="f-l-h">
+      <h4>Sneaker Company</h4>
+      <h1>Fall Limited Edition Sneakers</h1>
+    </div>
+
+    <div class="f-l-p">
+      <p>
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they'll withstand everything the
+        weather can offer.
+      </p>
+      <div>
+        <p>
+          <span>$125.00</span>
+          <span> 50%</span>
+        </p>
+        <p>$250.00</p>
+      </div>
+    </div>
+
+    <div class="f-l-b">
+      <div class="quantity">
+        <!-- <button></button> -->
+        <span> </span>
+        <!-- <button></button> -->
+      </div>
+      <button></button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: var(--very-dark-blue);
+}
+h4 {
+  color: var(--dark-grayish-blue);
+}
+</style>
